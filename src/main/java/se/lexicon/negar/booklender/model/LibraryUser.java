79e74576a05543bf8aafc074982da2fa7114d9d0
NegarMadadi <negar.myeah@gmail.com>
2021-01-01 00:19:04 -0800

    Added LibraryUser model

---
+++ src/main/java/se/lexicon/negar/booklender/model/LibraryUser.java
@@ -0,0 +1,81 @@
+package se.lexicon.negar.booklender.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+public class LibraryUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+
+    private LocalDate regDate;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    public LibraryUser() {
+    }
+
+    public LibraryUser(LocalDate regDate, String name, String email) {
+        this.regDate = regDate;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public LocalDate getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(LocalDate regDate) {
+        this.regDate = regDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LibraryUser that = (LibraryUser) o;
+        return userId == that.userId &&
+                Objects.equals(regDate, that.regDate) &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(email, that.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, regDate, name, email);
+    }
+
+    @Override
+    public String toString() {
+        return "LibraryUser{" +
+                "userId=" + userId +
+                ", RegDate=" + regDate +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/se/lexicon/negar/booklender/model/LibraryUserTest.java
@@ -0,0 +1,54 @@
+package se.lexicon.negar.booklender.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LibraryUserTest {
+
+    LibraryUser testLibraryUser;
+    @BeforeEach
+    void setUp() {
+        testLibraryUser = new LibraryUser(LocalDate.of(2021,1,1), "Negar","test@gmail.com");
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void successfully_created(){
+        assertNotNull(testLibraryUser);
+        assertTrue(testLibraryUser.getUserId() == 0);
+        assertEquals("Negar", testLibraryUser.getName());
+        assertEquals("test@gmail.com", testLibraryUser.getEmail());
+    }
+
+    @Test
+    void testEquals() {
+        LibraryUser copy = new LibraryUser(LocalDate.of(2021,1,1), "Negar", "test@gmail.com");
+
+        assertTrue(testLibraryUser.equals(copy));
+    }
+
+    @Test
+    void testHashcode() {
+        LibraryUser copy = new LibraryUser(LocalDate.of(2021,1,1), "Negar", "test@gmail.com");
+
+        assertEquals(copy.hashCode(), testLibraryUser.hashCode());
+    }
+
+    @Test
+    void testToString() {
+        String toString = testLibraryUser.toString();
+
+        assertTrue(toString.contains(Integer.toString(testLibraryUser.getUserId())));
+        assertTrue(toString.contains(testLibraryUser.getName()));
+        assertTrue(toString.contains(testLibraryUser.getEmail()));
+    }
+
+}
