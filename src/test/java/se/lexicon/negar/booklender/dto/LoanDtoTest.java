0f4462f0ce4e9121d86c4697c1d686c29080fe38
NegarMadadi <negar.myeah@gmail.com>
2021-01-04 10:03:24 -0800

    Removed unused packages

--- src/test/java/se/lexicon/negar/booklender/dto/LoanDtoTest.java
+++ src/test/java/se/lexicon/negar/booklender/dto/LoanDtoTest.java
@@ -9,7 +9,6 @@ import se.lexicon.negar.booklender.entity.Loan;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
